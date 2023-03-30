2261edb97edec078490ade63c842241290f956c5
Antonio Alan <tonio.alan07@gmail.com>
2023-03-30 16:27:53 -0300

    Criação do obj Bloco, Logo e component Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
-import { BrowserRouter as Router, Routes, Route, Link  } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom'
 import Home from './pages/Home'
 import Contact from './pages/Contact'
 import Company from './pages/Company'
 import NewProject from './pages/NewProject'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 
 function App() {
 
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/contact">Contato</Link>
-        <Link to="/newproject">Novo Projeto</Link>
-      </div>
+      <Navbar/>
       <Routes>
           <Route exact path='/' element={<Home/>}></Route>
           <Route exact path='/company' element={<Company/>}></Route>
           <Route exact path='/contact' element={<Contact/>}></Route>
           <Route exact path='/newproject' element={<NewProject/>}></Route>   
       </Routes>
-      <p>Footer</p>
+      <Footer/>
     </Router>
   )
 }
---
+++ src/objects/Logo/index.jsx
@@ -0,0 +1,9 @@
+import AppLogo from "../../assets/img/costs_logo.png"
+
+function Logo() {
+    return(
+        <img src={AppLogo} className="logo" alt="" />
+    )
+}
+
+export default Logo
