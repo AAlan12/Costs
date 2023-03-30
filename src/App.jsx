3d037cd11c6b58e6353a4ba5ee1de80bffe6de86
Antonio Alan <tonio.alan07@gmail.com>
2023-03-30 11:31:19 -0300

    Configuração de rotas para pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link  } from 'react-router-dom'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import Company from './pages/Company'
+import NewProject from './pages/NewProject'
+
 
 function App() {
 
   return (
-    <h1>Costs App</h1>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </div>
+      <Routes>
+          <Route exact path='/' element={<Home/>}></Route>
+          <Route exact path='/company' element={<Company/>}></Route>
+          <Route exact path='/contact' element={<Contact/>}></Route>
+          <Route exact path='/newproject' element={<NewProject/>}></Route>   
+      </Routes>
+      <p>Footer</p>
+    </Router>
   )
 }
 
