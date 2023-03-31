385a337cd0930bdc68a37c83cd4e4b97157e1bac
Antonio Alan <tonio.alan07@gmail.com>
2023-03-30 23:16:43 -0300

    Estruturação do page Home

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import styles from './styles.module.css'
+
+
+function Home() {
+    return (
+        <section className={styles.home_container}>
+            <h1>
+                Bem-vindo ao <span>Costs</span>
+            </h1>
+            <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+            
+        </section>
+    )
+}
+
+export default Home;
