f61dc6a5b0f45acd8a9c544860f03ecbea3476ed
Antonio Alan <tonio.alan07@gmail.com>
2023-03-31 02:46:16 -0300

    Init dá estruturação do page NewProject

---
+++ src/pages/NewProject/NewProject.jsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.css'
+function NewProject() {
+    return(
+        <div>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+        </div>
+    )
+}
+
+export default NewProject;
