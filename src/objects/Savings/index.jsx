4561f8ca3676d4da6748361ec3ac7d61adde3564
Antonio Alan <tonio.alan07@gmail.com>
2023-03-30 23:53:10 -0300

    Criação do obj Savings

---
+++ src/objects/Savings/index.jsx
@@ -0,0 +1,9 @@
+import SavingsImg from "../../assets/img/savings.svg"
+
+function Savings() {
+    return(
+        <img src={SavingsImg} alt="" />
+    )
+}
+
+export default Savings
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import styles from './styles.module.css'
+import Savings from "../../objects/Savings"
 
 
 function Home() {
@@ -8,6 +9,7 @@ function Home() {
                 Bem-vindo ao <span>Costs</span>
             </h1>
             <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+            <Savings/>
             
         </section>
     )
