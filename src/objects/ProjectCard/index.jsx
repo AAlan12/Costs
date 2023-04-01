e288143e68da65849e2db98b2e649326529ad409
Antonio Alan <tonio.alan07@gmail.com>
2023-03-31 21:46:34 -0300

    Criação do obj ProjectCard

---
+++ src/objects/ProjectCard/index.jsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.css';
+import { BsPencil, BsFillTrashFill } from 'react-icons/bs
+
+function ProjectCard({ id, name, budget, category, handleRemove }) {
+    return(
+        <></>
+    )
+}
+
+export default ProjectCard
--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,6 +1,8 @@
 import styles from './styles.module.css'
 import Message from '../../components/Message';
 import { useLocation } from 'react-router-dom'
+import Container from '../../objects/Container';
+import LinkButton from '../../objects/LinkButton';
 
 function Projects() {
 
@@ -9,10 +11,17 @@ function Projects() {
     if(location.state) {
         message = location.state.message
     }
+
     return(
-        <div>
-            <h1>Projects</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto"/>
+            </div>
             {message && <Message type="success" msg={message}/>}
+            <Container>
+                <p>Projetos...</p>
+            </Container>
         </div>
     )
 }
