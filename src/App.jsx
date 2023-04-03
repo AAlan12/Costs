3facf370782cda3ddeb57253693511a9ed8bb5c1
Antonio Alan <tonio.alan07@gmail.com>
2023-04-03 16:54:14 -0300

    Criação da rota e da page project

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Contact from './pages/Contact'
 import Company from './pages/Company'
 import NewProject from './pages/NewProject'
 import Projects from './pages/Projects'
+import Project from './pages/Project'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
@@ -15,10 +16,11 @@ function App() {
       <Navbar/>
       <Routes>
           <Route exact path='/' element={<Home/>}></Route>
-          <Route exact path='/company' element={<Company/>}></Route>
-          <Route exact path='/contact' element={<Contact/>}></Route>
-          <Route exact path='/newproject' element={<NewProject/>}></Route>   
-          <Route exact path='/projects' element={<Projects/>}></Route>   
+          <Route path='/company' element={<Company/>}></Route>
+          <Route path='/contact' element={<Contact/>}></Route>
+          <Route path='/newproject' element={<NewProject/>}></Route>   
+          <Route path='/projects' element={<Projects/>}></Route>   
+          <Route path='/project/:id' element={<Project/>}></Route>   
       </Routes>
       <Footer/>
     </Router>
