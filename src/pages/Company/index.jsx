97aaa87ee0bfed0e34ba41f205969bc8f4951272
Antonio Alan <tonio.alan07@gmail.com>
2023-04-04 17:02:10 -0300

    Styles da Page Company

--- src/pages/Company/index.jsx
+++ src/pages/Company/index.jsx
@@ -1,6 +1,20 @@
+import styles from './styles.module.css'
+import Container from '../../objects/Container'
+
 function Company() {
-    return(
-        <h1>Company</h1>
+    return (
+        <>
+            <div className={styles.company_details}>
+                <Container customClass="column">
+                    <div className={styles.company_container}>
+                        <h1>Nossa Empresa</h1>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum totam unde voluptatem. Fugit maxime cumque aspernatur omnis, eum natus placeat, labore vitae nulla corporis dolor, accusantium eius aliquid non molestias.</p>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum totam unde voluptatem. Fugit maxime cumque aspernatur omnis, eum natus placeat, labore vitae nulla corporis dolor, accusantium eius aliquid non molestias.</p>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum totam unde voluptatem. Fugit maxime cumque aspernatur omnis, eum natus placeat, labore vitae nulla corporis dolor, accusantium eius aliquid non molestias.</p>
+                    </div>
+                </Container>
+            </div>
+        </>
     )
 }
 
