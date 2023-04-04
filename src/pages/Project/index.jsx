80a2bcd7663dac9606712abcf19dad82e7ba805b
Antonio Alan <tonio.alan07@gmail.com>
2023-04-04 00:55:34 -0300

    function do editPost no Page Project

--- src/pages/Project/index.jsx
+++ src/pages/Project/index.jsx
@@ -11,9 +11,14 @@ import ServiceForm from '../../components/ServiceForm'
 
 
 function Project() {
-    
+
     const { id } = useParams()
     const [project, setProject] = useState([])
+    const [showProjectForm, setShowProjectForm] = useState(false)
+    const [showServiceForm, setShowServiceForm] = useState(false)
+    const [services, setServices] = useState([])
+    const [message, setMessage] = useState('')
+    const [type, setType] = useState('success')
 
     useEffect(() => {
         fetch(`http://localhost:5000/projects/${id}`, {
@@ -22,14 +27,39 @@ function Project() {
                 'Content-Type': 'application/json',
             },
         })
-        .then((resp) => resp.json())
-        .then((data) => {
-            setProject(data)
-        })
-        .catch((err) => console.log)
+            .then((resp) => resp.json())
+            .then((data) => {
+                setProject(data)
+                setServices(data.services)
+            })
+            .catch((err) => console.log)
     }, [id])
 
-    return(
+    function editPost(project) {
+        // budget validation
+        if (project.budget < project.cost) {
+            setMessage('O Orçamento não pode ser menor que o custo do projeto!')
+            setType('error')
+            return false
+        }
+
+        fetch(`http://localhost:5000/projects/${project.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(project),
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setProject(data)
+                setShowProjectForm(!showProjectForm)
+                setMessage('Projeto atualizado!')
+                setType('success')
+            })
+    }
+
+    return (
         <p>{project.name}</p>
     )
 }
