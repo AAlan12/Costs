99761681b26616815a8e8e6546efac6659a724b9
Antonio Alan <tonio.alan07@gmail.com>
2023-04-04 09:57:14 -0300

    Finalização da Page Project

--- src/pages/Project/index.jsx
+++ src/pages/Project/index.jsx
@@ -123,14 +123,81 @@ function Project() {
 
     function toggleProjectForm() {
         setShowProjectForm(!showProjectForm)
-      }
-    
-      function toggleServiceForm() {
+    }
+
+    function toggleServiceForm() {
         setShowServiceForm(!showServiceForm)
-      }
+    }
 
     return (
-        <p>{project.name}</p>
+        <>
+            {project.name ? (
+                <div className={styles.project_details}>
+                    <Container customClass="column">
+                        {message && <Message type={type} msg={message} />}
+                        <div className={styles.details_container}>
+                            <h1>Projeto: {project.name}</h1>
+                            <button className={styles.btn} onClick={toggleProjectForm}>
+                                {!showProjectForm ? 'Editar projeto' : 'Fechar'}
+                            </button>
+                            {!showProjectForm ? (
+                                <div className={styles.form}>
+                                    <p>
+                                        <span>Categoria:</span> {project.category.name}
+                                    </p>
+                                    <p>
+                                        <span>Total do orçamento:</span> R${project.budget}
+                                    </p>
+                                    <p>
+                                        <span>Total utilizado:</span> R${project.cost}
+                                    </p>
+                                </div>
+                            ) : (
+                                <div className={styles.form}>
+                                    <ProjectForm
+                                        handleSubmit={editPost}
+                                        btnText="Concluir Edição"
+                                        projectData={project}
+                                    />
+                                </div>
+                            )}
+                        </div>
+                        <div className={styles.service_form_container}>
+                            <h2>Adicione um serviço:</h2>
+                            <button className={styles.btn} onClick={toggleServiceForm}>
+                                {!showServiceForm ? 'Adicionar Serviço' : 'Fechar'}
+                            </button>
+                            <div className={styles.form}>
+                                {showServiceForm && (
+                                    <ServiceForm
+                                        handleSubmit={createService}
+                                        btnText="Adicionar Serviço"
+                                        projectData={project}
+                                    />
+                                )}
+                            </div>
+                        </div>
+                        <h2>Serviços:</h2>
+                        <Container customClass="start">
+                            {services.length > 0 &&
+                                services.map((service) => (
+                                    <ServiceCard
+                                        id={service.id}
+                                        name={service.name}
+                                        cost={service.cost}
+                                        description={service.description}
+                                        key={service.id}
+                                        handleRemove={removeService}
+                                    />
+                                ))}
+                            {services.length === 0 && <p>Não há serviços cadastrados.</p>}
+                        </Container>
+                    </Container>
+                </div>
+            ) : (
+                <Loading />
+            )}
+        </>
     )
 }
 
