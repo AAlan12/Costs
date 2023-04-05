944e3e9403f3d182c7f37dc5c6e79d7ba19028b8
Antonio Alan <tonio.alan07@gmail.com>
2023-04-05 19:13:48 -0300

    Criação do obj ContactCard

---
+++ src/objects/ContactCard/index.jsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css';
+
+function ContactCard() {
+    return(
+        <></>
+    )
+}
+
+export default ContactCard
--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,10 +1,16 @@
 import styles from './styles.module.css'
+import Container from '../../objects/Container';
 
 function Contact() {
     return(
-        <>
-            <h1>Contatos</h1>
-        </>
+        <div className={styles.contact_container}>
+            <div className={styles.title_container}>
+                <h1>Contatos</h1>  
+            </div>
+            <Container customClass="start">
+                
+            </Container>
+        </div>
     )
 }
 
